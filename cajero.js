5db4745c0e95678f911a567ae52f7163f60fc40e
carlos <casilva2@utpl.edu.ec>
2020-01-19 21:22:23 -0500

    archivo agregados atm.html cajero.gif cajero.js

---
+++ cajero.js
@@ -0,0 +1,68 @@
+class Billete
+{
+  constructor(v, c)
+  {
+    this.valor = v;
+    this.cantidad = c;
+  }
+}
+
+function entregarDinero()
+{
+  var t = document.getElementById("dinero");
+  dinero = parseInt(t.value);
+  for(var bi of caja)
+  {
+
+    if(dinero > 0)
+    {
+      div = Math.floor(dinero / bi.valor);
+
+      if(div > bi.cantidad)
+      {
+        papeles = bi.cantidad;
+      }
+      else
+      {
+        papeles = div;
+      }
+
+      entregado.push( new Billete(bi.valor, papeles) );
+      dinero = dinero - (bi.valor * papeles);
+    }
+
+  }
+
+  if(dinero > 0)
+  {
+    resultado.innerHTML = "No hay esa cantidad";
+  }
+  else
+  {
+    for(var e of entregado)
+    {
+      if(e.cantidad > 0)
+      {
+              resultado.innerHTML += e.cantidad + " billetes de $" + e.valor + "<br />";
+      }
+    }
+  }
+}
+
+var caja = [];
+var entregado = [];
+caja.push( new Billete(100, 50) );//5000
+caja.push( new Billete(50, 30) );//1500
+caja.push( new Billete(20, 100) );//2000
+caja.push( new Billete(10, 100) );//1000
+caja.push( new Billete(5, 100) );//500
+
+//para este caso cajero se carga con con $ 10.000
+
+var dinero = 0;
+var div = 0;
+var papeles = 0;
+
+var resultado = document.getElementById("resultado");
+var b = document.getElementById("extraer");
+b.addEventListener("click", entregarDinero);
